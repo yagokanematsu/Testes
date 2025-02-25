4ead6eeca9976ff9bad0a837cb1723bd74239502
Yago Kanematsu <154649751+yagokanematsu@users.noreply.github.com>
2025-02-25 20:47:39 -0300

    Update dado.py

--- dado.py
+++ dado.py
@@ -3,29 +3,29 @@ import random
 from PIL import Image, ImageTk
 
 def aleatorio():
-    global photo 
+    global foto
     numero = random.randint(1, 6)
-    image_caminho = f'C:\\Programas\\dado{numero}.jpg'
-    image = Image.open(image_caminho)
-    photo = ImageTk.PhotoImage(image)  
-    label.config(image=photo)
+    imagem_caminho = f'C:\\Programas\\dado{numero}.jpg'
+    imagem = Image.open(imagem_caminho)
+    foto = ImageTk.PhotoImage(imagem)  
+    espaco.config(image=foto)
 
 aba = tk.Tk()
 aba.geometry('500x300')
-aba.title('Bloco')
+aba.title('Dado')
 
 quadrado = tk.Frame(aba, bg="#ffffff", padx=100, pady=50)
 quadrado.pack(pady=40)
 
-image_caminho = "C:\\Programas\\dado6.jpg"
-image = Image.open(image_caminho)
-photo = ImageTk.PhotoImage(image)
+imagem_caminho = "C:\\Programas\\branco.jpg"
+imagem = Image.open(imagem_caminho)
+foto = ImageTk.PhotoImage(imagem)
 
 botao = tk.Button(quadrado, text="Rolar o dado", command=aleatorio, bg="#4163f8",font=('Arial', 10),fg='white',width=12)
 botao.grid(column=1, row=1, columnspan=2)
 
 
-label = tk.Label(quadrado, image=photo)
-label.grid(row=0, column=1,columnspan=2)
+espaco = tk.Label(quadrado, image=foto)
+espaco.grid(row=0, column=1,columnspan=2)
 
 aba.mainloop()

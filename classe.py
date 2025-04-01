3076cb92fa61268552077849ef415532b80aa408
Yago Kanematsu <yakanematsu@gmail.com>
2025-04-01 02:24:20 -0300

    Estudando

--- classe.py
+++ classe.py
@@ -1,38 +1,44 @@
-def marca(self):
-    return self.marca 
+from abc import ABC, abstractmethod
 
-def mudar_marca(self, nova_marca):
-    self.marca = nova_marca
-
-class Transporte:
-    def __init__(self, marca, modelo, ano, consumo):
+class transporte(ABC):
+    def __init__(self, marca, modelo, ano):
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
-        self.consumo = consumo
-class Passeio(Transporte):
-    def __init__(self, marca, modelo, ano, consumo, capacidade):
-        super().__init__(marca, modelo, ano, consumo)
+
+        @abstractmethod
+        def __str__():
+            pass
+
+        @abstractmethod
+        def mudar_capacidade(self):
+            pass
+
+        @abstractmethod
+        def mudar_carga(self):
+            pass
+
+class carro(transporte):
+    def __init__(self, marca, modelo, ano, capacidade):
+        super().__init__(marca, modelo, ano)
         self.capacidade = capacidade
-    def calcular_autonomia(self):
-        return self.consumo*self.capacidade
+
     def __str__(self):
         autonomia = self.calcular_autonomia()
-        return f"Um {self.marca} {self.modelo} {self.ano}, com consumo médio de {self.consumo} km/l e capacidade do tanque de {self.capacidade} L, tem autonomia de {autonomia} km"
-carro = Passeio("Honda", "Civic", 2020, 12, 50)
-print(carro)
+        return f"Um {self.marca} {self.modelo} {self.ano}, com capacidade do tanque de {self.capacidade} L, tem autonomia de {autonomia} km"
 
-class Caminhao(Transporte):
-    def __init__(self, marca, modelo, ano, consumo, carga):
-        super().__init__(marca, modelo, ano, consumo)
+    def mudar_carga(self):
+        self.carga = float(input())
+
+
+
+class Caminhao(transporte):
+    def __init__(self, marca, modelo, ano, carga):
+        super().__init__(marca, modelo, ano)
         self.carga = carga
-    def calcular_custo_viagem(self, distancia, combustivel):
-        a = (distancia/self.consumo)*combustivel
-        return a
+
     def __str__(self):
-        return f"Um caminhão {self.marca} {self.modelo}, {self.ano}, com consumo de {self.consumo} km/L carregando {self.carga:.2f} kgs"
-caminhao1 = Caminhao("Volvo", "FH", 2021, 4, 20000.0000000)
-print(caminhao1)
-print(marca(caminhao1))
-mudar_marca(caminhao1, 'zzzzz')
-print(marca(caminhao1))
+        return f"Um caminhão {self.marca} {self.modelo}, {self.ano}, carregando {self.carga:.2f} kgs"
+    
+    def mudar_carga(self):
+        self.carga = float(input())
